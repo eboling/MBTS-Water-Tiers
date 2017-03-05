ecf21b8af4ff84a9ef2bdfaf86026ba75e246602
Eli Boling <eboling@draper.com>
2017-03-05 14:13:22 -0500

    Improving graph UI.

--- water_rates.py
+++ water_rates.py
@@ -23,7 +23,7 @@ class ViewRect:
     def map_y(self, y):
         return self.outer_height - self.offset_y(y)
 
-class Sampler:
+class LinearMapping:
     def __init__(self, x, x_prime):
         self.denom = float(x[1] - x[0])
         self.scale = float(x_prime[1] - x_prime[0])
@@ -34,9 +34,13 @@ class Sampler:
     def f_inverse(self, x):
         return float(x) / self.inverse_denom * self.inverse_scale
 
+AXIS_LABEL_MARGIN = 75
+MAJOR_TICK_SIZE = 8
+    
 def draw_axes(canvas, values, graph_rect):
-    y_sampler = Sampler((0, values[len(values) - 1]), (0, graph_rect.height()))
-    x_sampler = Sampler((0, len(values)), (0, graph_rect.width()))
+    #y_sampler = LinearMapping((0, values[len(values) - 1]), (0, graph_rect.height()))
+    y_sampler = LinearMapping((0, 2500), (0, graph_rect.height()))
+    x_sampler = LinearMapping((0, len(values)), (0, graph_rect.width()))
     x0 = graph_rect.map_x(0)
     xmax = graph_rect.map_x(graph_rect.width())
     y0 = graph_rect.map_y(0)
@@ -46,46 +50,43 @@ def draw_axes(canvas, values, graph_rect):
     x_step = graph_rect.width() / 10
     for xin in xrange(0, graph_rect.width(), x_step):
         x = graph_rect.map_x(xin)
-        canvas.create_line(x, y0, x, y0 + 8, fill="black")
+        canvas.create_line(x, y0, x, y0 + MAJOR_TICK_SIZE, fill="black")
         x_val = x_sampler.f_inverse(xin)
         canvas.create_text(x, y0 + 10, text="{0}".format(int(x_val)), anchor = tk.NW)
-    y_step = int(values[len(values) - 1] / 10)
-    for yin in xrange(0, int(values[len(values) - 1]), y_step):
+    #y_step = int(values[len(values) - 1] / 10)
+    #for yin in xrange(0, int(values[len(values) - 1]), y_step):
+    y_step = int(2500 / 10)
+    for yin in xrange(0, 2500, y_step):
         y_val = yin
         y = graph_rect.map_y(y_sampler.f(y_val))
-        canvas.create_line(x0 - 8, y, x0, y, fill="black")
-        canvas.create_text(x0 - 8 - 75, y, text="{0}".format(int(y_val)), anchor = tk.NW)
-        
+        canvas.create_line(x0 - MAJOR_TICK_SIZE, y, x0, y, fill="black")
+        canvas.create_text(x0 - MAJOR_TICK_SIZE - AXIS_LABEL_MARGIN, y, text="{0}".format(int(y_val)), anchor = tk.NW)
+
 def do_graph(canvas, values, xMargin, yMargin):
     canvas_width =  800
     canvas_height = 500
     graph_rect = ViewRect(canvas_width, canvas_height,
-                              (xMargin + 75, yMargin), (canvas_width - xMargin, canvas_height - yMargin))
+                              (xMargin + AXIS_LABEL_MARGIN, yMargin), (canvas_width - xMargin, canvas_height - yMargin))
     #canvas_width = canvas.winfo_width();
     #canvas_height = canvas.winfo_height();
+    x_mapping = LinearMapping((0, graph_rect.width()), (0, len(values) - 1))
+    #y_mapping = LinearMapping((0, values[len(values) - 1]), (0, graph_rect.height()))
+    y_mapping = LinearMapping((0, 2500), (0, graph_rect.height()))
     values.sort()
     draw_axes(canvas, values, graph_rect)
-    #graph_interior_width = canvas_width - xMargin * 2
-    #graph_interior_height = canvas_height - yMargin * 2
-    if graph_rect.width() > len(values):
-        sampling_step = 1
-    else:
-        sampling_step = float(len(values)) / float(graph_rect.width())
-    max_usage = values[len(values) - 1]
-    index = 0.0;
     x0 = graph_rect.map_x(0)
     y0 = graph_rect.map_y(0)
-    #max_x = graph_rect.offset_x(graph_rect.width())
-    #max_y = graph_rect.offset_y(graph_rect.height())
-    max_y = graph_rect.height()
-    for x1 in xrange (graph_rect.map_x(0), graph_rect.map_x(graph_rect.width())):
-        y = values[int(index)]
-        y1 = graph_rect.map_y(int((float(y) / max_usage) * float(max_y)))
+    x_step = 5
+    def render_segment(x_val, x0, y0):
+        usage = values[int(x_mapping.f(x_val))]
+        y_val = y_mapping.f(usage);
+        x1 = graph_rect.map_x(x_val);
+        y1 = graph_rect.map_y(y_val);
         canvas.create_line(x0, y0, x1, y1, fill="black")
-        #print "({0}, {1}) - ({2}, {3})".format(x0, y0, x1, y1)
-        x0 = x1
-        y0 = y1
-        index = index + sampling_step
+        return x1, y1
+    for x_val in xrange(x_step, graph_rect.width(), x_step):
+        x0, y0 = render_segment(x_val, x0, y0)
+    x0, y0 = render_segment(graph_rect.width(), x0, y0)
         
 raw_data_dir = sys.argv[1]
 raw_Qs = RawQData(raw_data_dir)
