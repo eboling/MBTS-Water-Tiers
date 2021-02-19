45aeec2cfd0c81eaae0cdae419eb377f5cea1da2
Eli Boling <eboling@gmail.com>
2021-02-19 08:49:17 -0500

    Added conversion adapter support to data bindings.

--- data_bindings.py
+++ data_bindings.py
@@ -1,4 +1,23 @@
 
+class ConversionAdapter:
+    def __init__(self, push, pull):
+        self.push = push
+        self.pull = pull
+
+NopAdapter = ConversionAdapter(push=lambda x: x, pull=lambda x: x)
+FloatToInt = ConversionAdapter(push=lambda x: int(x), pull=lambda x: float(x))
+
+class Binding:
+    def __init__(self, getter, setter = lambda: None):
+        self.getter = getter
+        self.setter = setter
+        
+    def get(self):
+        return self.getter()
+    
+    def set(self, value):
+        self.setter(value)
+
 class Facet:
     def __init__(self, obj, field):
         self.obj = obj
@@ -14,13 +33,13 @@ class DataBindings:
     def __init__(self):
         self.__bindings = []
 
-    def add_binding(self, source, destination):
-        self.__bindings.append((source, destination))
+    def add_binding(self, source, destination, conversion = NopAdapter):
+        self.__bindings.append((source, destination, conversion))
 
     def push(self):
         for b in self.__bindings:
-            b[1].set(b[0].get())
+            b[1].set(b[2].push(b[0].get()))
             
     def pull(self):
         for b in self.__bindings:
-            b[0].set(b[1].get())
+            b[0].set(b[2].pull(b[1].get()))
